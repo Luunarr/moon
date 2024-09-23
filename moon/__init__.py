357bbd13fe1300036333878b47e6c1617f992cf5
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-23 18:28:37 +0200

    Add files via upload

---
+++ moon/__init__.py
@@ -0,0 +1,9 @@
+# ._________________________________________.
+# |                                         |
+# | Lunar : https://github.com/Luunarr      |
+# |_________________________________________|
+# |                                         |
+# | Moon  : https://github.com/Luunarr/moon |
+# |_________________________________________|
+
+from .moon import Cmoon, Smoon, Bmoon, Emoon
